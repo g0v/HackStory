a72957a96f31f0bbdb888a7be476c2b1d1951cdd
MrOrz <johnsonliang7@gmail.com>
2016-08-06 16:16:55 +0800

    Reads column name from google sheet

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,8 @@
 // Your Client ID can be retrieved from your project in the Google
 // Developer Console, https://console.developers.google.com
 var CLIENT_ID = '540045535516-bd21gnjkb2g2rsof4bah1p3i904njtk0.apps.googleusercontent.com';
-
+var SPREADSHEET_ID = getFromSearchString('source') || '1Pn6E321fuwnrvgoLrl6Qsrb2aPbL9hN9ahptedPtSQE';
+var SHEET_NAMES = (getFromSearchString('sheets') || 'od1').split(',');
 var SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
 var loginButton = document.querySelector('#login');
@@ -24,7 +25,7 @@ function authorize(immediate) {
       // Load sheet api now.
       //
       gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4')
-        .then(listMajors);
+        .then(fetchColumnHeaders);
     } else {
       // enable login button.
       loginButton.removeAttribute('disabled');
@@ -36,18 +37,36 @@ loginButton.addEventListener('click', function () {
   authorize(false);
 })
 
+var columnNameForSheets = {};
+
 /**
  * Print the names and majors of students in a sample spreadsheet:
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
-function listMajors() {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
+function fetchColumnHeaders() {
+  gapi.client.sheets.spreadsheets.values.batchGet({
+    spreadsheetId: SPREADSHEET_ID,
+    ranges: SHEET_NAMES.map(function(name){
+      return `${name}!A1:1`
+    }),
   }).then(function(response) {
-    var range = response.result;
-    console.log(range)
+    response.result.valueRanges.forEach(function(data, i){
+      var sheetName = SHEET_NAMES[i];
+      var values = data.values;
+      columnNameForSheets[sheetName] = values[0];
+    });
+
+    console.log(columnNameForSheets)
   }, function(response) {
     console.error(response.result.error.message)
   });
+}
+
+function addEvent() {
+
+}
+
+function getFromSearchString(key) {
+  var matches = (RegExp(`${key}=([^&]+)`)).exec(location.search);
+  return matches && matches[1];
 }
