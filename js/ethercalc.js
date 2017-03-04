add7d8917a55135ff47d77b6e7404fdb9739c12f
Sasaya <storyn26383@gmail.com>
2017-03-04 16:56:42 +0800

    init ec client

--- js/ethercalc.js
+++ js/ethercalc.js
@@ -1,1 +1,8 @@
-$.get('https://ethercalc.org/q3knnr6tfkqw.1.csv.json', data => console.log(data));
+window.EC = {
+  get: (id, callback) => {
+    $.get('https://ethercalc.org/' + id + '.csv.json', callback);
+  }
+};
+
+EC.get('q3knnr6tfkqw', data => console.log(data));
+EC.get('q3knnr6tfkqw.1', data => console.log(data));
